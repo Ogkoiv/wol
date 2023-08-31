56155f086209a20bd6da2869b586bac07a66a2d1
AB6914 <AB6914@student.jamk.fi>
2023-08-31 12:43:15 +0300

    making this work

---
+++ send_package.py
@@ -0,0 +1,11 @@
+def construct_package(mac):
+    array=[255]*6
+    mac = mac.split(':')
+    mac = [int(x, 16) for x in mac]
+    mac=mac*16
+    array.extend(mac)
+    finalarray=array
+    return finalarray
+
+
+
