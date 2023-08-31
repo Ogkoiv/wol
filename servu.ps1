10252d95840926e98f16ec2b03c549de00e4e801
AB6914 <AB6914@student.jamk.fi>
2023-08-31 13:37:13 +0300

    ps1_to_all

--- servu.ps1
+++ servu.ps1
@@ -1,8 +1,8 @@
 Write-Host("Starting script")
-Set-Location -Path C:\Users\Ossi\wol
-python3 wol.py servu
+Set-Location -Path C:\Users\Username\wol
+python3 wol.py servu #servu is alias for the ssh connection, use your own connection settings here
 Write-Host("magic packet was sent waiting for startup")
 Start-Sleep -Seconds 180
 ssh servu
-Set-Location -Path C:\Users\Ossi
+Set-Location -Path C:\Users\Username
 exit(1)
